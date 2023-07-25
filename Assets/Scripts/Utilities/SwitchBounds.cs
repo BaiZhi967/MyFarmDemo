5882dda193699c329fb8a5f5c2cfbbef5070aa72
whitezhi <moycx@qq.com>
2023-07-25 17:39:15 +0800

    摄像机跟随 三个树的预制体 边界碰撞体 景观物体遮挡半透明 背包数据初始化

---
+++ Assets/Scripts/Utilities/SwitchBounds.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+
+    private void SwitchConfinerShape()
+    {
+        PolygonCollider2D confinerShape = GameObject.FindGameObjectWithTag("BoundsConfiner").GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner confiner = GetComponent<CinemachineConfiner>();
+
+        confiner.m_BoundingShape2D = confinerShape;
+
+        //Call this if the bounding shape's points change at runtime
+        confiner.InvalidatePathCache();
+    }
+}
