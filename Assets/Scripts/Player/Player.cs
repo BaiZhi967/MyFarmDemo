bab6a9750f183d9449c53d28003decd8fdbe4419
BaiZhi967 <moycx@qq.com>
2023-07-29 06:09:21 +0800

    制作 Player 的动画

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,15 +10,19 @@ public class Player : MonoBehaviour
     private float inputY;
     public float speed;
     private Vector2 movementInput;
+    private Animator[] _animators;
+    private bool isMoving = false;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animators = GetComponentsInChildren<Animator>();
     }
 
     private void Update()
     {
         PlayerInput();
+        SwitchAnimation();
     }
 
     private void FixedUpdate()
@@ -35,11 +39,31 @@ public class Player : MonoBehaviour
             inputX *= 0.6f;
             inputY *= 0.6f;
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            inputX *= 0.5f;
+            inputY *= 0.5f;
+        }
         movementInput = new Vector2(inputX, inputY);
+        isMoving = movementInput != Vector2.zero;
     }
 
     private void Movement()
     {
         _rigidbody2D.MovePosition(_rigidbody2D.position + movementInput * speed * Time.deltaTime);
+    }
+
+    private void SwitchAnimation()
+    {
+        foreach (var animator in _animators)
+        {
+            animator.SetBool("isMoving",isMoving);
+            if (isMoving)
+            {
+                animator.SetFloat("InputX",inputX);
+                animator.SetFloat("InputY",inputY);
+            }
+        }
     }
 }
