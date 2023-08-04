41f945e9941b5463772b516dddb4e7feb159676d
whitezhi <moycx@qq.com>
2023-08-04 11:05:15 +0800

    石头粒子特效

--- Assets/Scripts/ObjectPool/PoolManager.cs
+++ Assets/Scripts/ObjectPool/PoolManager.cs
@@ -54,6 +54,7 @@ public class PoolManager : MonoBehaviour
         {
             ParticleEffectType.LeavesFalling01 => poolEffectList[0],
             ParticleEffectType.LeavesFalling02 => poolEffectList[1],
+            ParticleEffectType.Rock => poolEffectList[2],
             _ => null,
         };
 
