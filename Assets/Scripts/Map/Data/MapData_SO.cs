d8ba86340d1cbd665bc8a7278b0e02c8fbac0af1
whitezhi <moycx@qq.com>
2023-08-04 13:39:08 +0800

    （AStar）基础数据创建 Node & GridNodes

--- Assets/Scripts/Map/Data/MapData_SO.cs
+++ Assets/Scripts/Map/Data/MapData_SO.cs
@@ -9,5 +9,12 @@ public class MapData_SO : ScriptableObject
     [SceneName]
     public string sceneName;
 
+    [Header("场景大小")] 
+    public int gridWidth;
+    public int gridHeight;
+    [Header("左下原点")] 
+    public int originX;
+    public int originY;
+
     public List<TileProperty> tileProperties;
 }
