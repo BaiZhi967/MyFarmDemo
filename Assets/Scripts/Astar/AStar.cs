6f3f083d6e4bc05451ff5410654b8e5dec930f64
whitezhi <moycx@qq.com>
2023-08-04 14:05:18 +0800

    （AStar）核心功能评估周围节点得到最短路径

--- Assets/Scripts/Astar/AStar.cs
+++ Assets/Scripts/Astar/AStar.cs
@@ -30,7 +30,7 @@ namespace WzFarm.AStar
                 //查找最短路径
                 if (FindShortestPath())
                 {
-                    //构建NPC移动路径}
+                    //构建NPC移动路径
                 }
             }
         }
@@ -109,9 +109,82 @@ namespace WzFarm.AStar
                 }
 
                 //计算周围8个Node补充到OpenList
+                EvaluateNeighbourNodes(closeNode);
             }
 
             return pathFound;
+        }
+        
+          /// <summary>
+        /// 评估周围8个点，并生成对应消耗值
+        /// </summary>
+        /// <param name="currentNode"></param>
+        private void EvaluateNeighbourNodes(Node currentNode)
+        {
+            Vector2Int currentNodePos = currentNode.gridPosition;
+            Node validNeighbourNode;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    validNeighbourNode = GetValidNeighbourNode(currentNodePos.x + x, currentNodePos.y + y);
+
+                    if (validNeighbourNode != null)
+                    {
+                        if (!openNodeList.Contains(validNeighbourNode))
+                        {
+                            validNeighbourNode.gCost = currentNode.gCost + GetDistance(currentNode, validNeighbourNode);
+                            validNeighbourNode.hCost = GetDistance(validNeighbourNode, targetNode);
+                            //链接父节点
+                            validNeighbourNode.parentNode = currentNode;
+                            openNodeList.Add(validNeighbourNode);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 找到有效的Node,非障碍，非已选择
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Node GetValidNeighbourNode(int x, int y)
+        {
+            if (x >= gridWidth || y >= gridHeight || x < 0 || y < 0)
+                return null;
+
+            Node neighbourNode = gridNodes.GetGridNode(x, y);
+
+            if (neighbourNode.isObstacle || closedNodeList.Contains(neighbourNode))
+                return null;
+            else
+                return neighbourNode;
+        }
+
+
+        /// <summary>
+        /// 返回两点距离值
+        /// </summary>
+        /// <param name="nodeA"></param>
+        /// <param name="nodeB"></param>
+        /// <returns>14的倍数+10的倍数</returns>
+        private int GetDistance(Node nodeA, Node nodeB)
+        {
+            int xDistance = Mathf.Abs(nodeA.gridPosition.x - nodeB.gridPosition.x);
+            int yDistance = Mathf.Abs(nodeA.gridPosition.y - nodeB.gridPosition.y);
+
+            if (xDistance > yDistance)
+            {
+                return 14 * yDistance + 10 * (xDistance - yDistance);
+            }
+            return 14 * xDistance + 10 * (yDistance - xDistance);
         }
     }
 }
